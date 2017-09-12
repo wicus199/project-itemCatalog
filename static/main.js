40abe798d573b3d96afc7e813a2ba0fac6e55d73
Wicus van der Westhuizen <wicus92@gmail.com>
2017-09-12 20:02:39 +0200

    Add initial javascript file used to animate the side navigation bar.

---
+++ static/main.js
@@ -0,0 +1,9 @@
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+function openNav() {
+    document.getElementById("sideMenu").style.width = "250px";
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+function closeNav() {
+    document.getElementById("sideMenu").style.width = "0";
+}
