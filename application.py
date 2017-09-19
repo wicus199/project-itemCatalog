77a12051c8000c1925c8a6113d7a90ace78d27ae
Wicus van der Westhuizen <wicus92@gmail.com>
2017-09-19 21:56:55 +0200

    Add functions in python file to view catalog, categories, and items inside categories.

--- application.py
+++ application.py
@@ -20,7 +20,7 @@ Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
-# Main catalog page
+# Main catalog page -- Methods: GET
 @app.route('/')
 @app.route('/catalog/')
 def catalog():
@@ -29,42 +29,51 @@ def catalog():
     # Pass main_catalog to catalog.html page (main page) into catalog_items
     return render_template('catalog.html', catalog_items=main_catalog)
 
-# Create a new catalog category
-@app.route('/catagory/new/')
-def createNewCategory():
-    return
+# # Add a category -- Methods: GET, POST
+# @app.route('/catalog/add/')
+# def addCategory():
+#     return
 
-# View the contents of a category
-@app.route('/catagory/<int:catagory_id>/')
-def viewCategory():
-    return
+# View the contents of a category -- Methods: GET
+@app.route('/catalog/<int:category_id>/')
+def viewCategory(category_id):
+    # Query the items in a category
+    category = session.query(Category).filter_by(id=category_id).one()
+    items = session.query(Item).filter_by(category_id=category.id).all()
+    return render_template('category.html', category=category, itemList=items)
 
-# Edit a catagory
-@app.route('/catagory/<int:catagory_id>/edit/')
-def editCategory():
-    return
+# # Edit a category -- Methods: GET, POST
+# @app.route('/catalog/<int:category_id>/edit/')
+# def editCategory():
+#     return
+#
+# # Delete a category -- Methods: GET, POST
+# @app.route('/catalog/<int:category_id>/delete/')
+# def deleteCatagory():
+#     return
+#
+# View an item -- Methods: GET
+@app.route('/catalog/<int:category_id>/item/<int:item_id>/')
+def viewItem(category_id, item_id):
+    category = session.query(Category).filter_by(id=category_id).one()
+    item = session.query(Item).filter_by(id=item_id).one()
+    return render_template('items.html', category=category, item=item)
 
-# Delete a category
-@app.route('/catagory/<int:catagory_id>/delete/')
-def deleteCatagory():
-    return
+# # Create a new item inside a category -- Methods: GET, POST
+# @app.route('/catalog/<int:category_id>/add/')
+# def createNewItem():
+#     return
+#
+# # Edit an item -- Methods: GET, POST
+# @app.route('/catalog/<int:category_id>/item/<int:item_id>/edit/')
+# def editItem():
+#     return
+#
+# # Delete an item -- Methods: GET, POST
+# @app.route('/catalog/<int:category_id>/item/<int:item_id>/delete/')
+# def deleteItem():
+#     return
 
-# Create a new item inside a category
-@app.route('/catagory/<int:catagory_id>/new/')
-def createNewItem():
-    return
-
-# View an item
-@app.route('/catagory/<int:catagory_id>/item/<int:item_id>/')
-def viewItem():
-    return
-
-# Edit an item
-@app.route('/catagory/<int:catagory_id>/item/<int:item_id>/edit/')
-def editItem():
-    return
-
-# Delete an item
-@app.route('/catagory/<int:catagory_id>/item/<int:item_id>/delete')
-def deleteItem():
-    return
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host='0.0.0.0', port=5000)
